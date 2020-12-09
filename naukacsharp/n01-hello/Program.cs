cec28befcf2eb2f6fb1bae2282390b5c8a1a80f9
patora58 <patora58@gmail.com>
2020-12-09 21:29:38 +0100

    hello world

---
+++ naukacsharp/n01-hello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace n01_hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
