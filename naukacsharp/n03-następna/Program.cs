3f75099a6c44b5da541f59ddb76df893a2378ecc
patora58 <patora58@gmail.com>
2020-12-12 16:54:25 +0100

    nastepna parzysta i nieparzysta - if/else

---
+++ naukacsharp/n03-następna/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace n03_następna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Następna liczba");
+            //1. pobranie liczby od użytkownika
+            Console.Write("podaj liczbe: ");
+            string odpowiedz = Console.ReadLine();
+            int podana = Convert.ToInt32(odpowiedz);
+            //2. wywołanie funkcji do określenia nastepnej lczby nieparzystej
+            int nieparzysta = NastepnaNieparzysta(podana);
+            //3. wywołanie funkcji do określenia nastepnej lczby parzystej
+            int parzysta = NastepnaParzysta(podana);
+            //4. wyświetlenie wyników
+            Console.WriteLine("nieparzysta=" + nieparzysta);
+            Console.WriteLine("parzysta=" + parzysta);
+
+            //Console.ReadLine();
+        }
+
+        static int NastepnaParzysta(int liczba)
+        {
+            int wynik = liczba;
+            if (liczba % 2 == 0)
+            {
+                wynik = wynik + 2;
+            }
+            else
+            {
+                wynik = wynik + 1;
+            }
+
+            return wynik;
+           
+
+        }
+
+        static int NastepnaNieparzysta(int liczba)
+        {
+            int wynik = liczba;
+            if (liczba % 2 == 0)
+            {
+               wynik = wynik + 1;
+            }
+            else
+            {
+                wynik = wynik + 2;
+            }
+
+            return wynik;
+        }
+
+        
+
+
+    }
+}
