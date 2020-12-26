6cc8589262e95933222aac94f47051af70f67ed1
patora58 <patora58@gmail.com>
2020-12-26 11:16:56 +0100

    Klasa Gwiazdki z algorytmem

---
+++ naukacsharp/n05-gwiazdki/Gwiazdki.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace n05_gwiazdki
+{
+    public class Gwiazdki
+    {
+        const int Wiersze = 3;
+        /*
+       ------------+
+     1|   *     D) |
+     2|  **        |
+     3| ***        |
+     4|****        |
+        */
+
+        public void GwiazdkiD()
+        {
+            int wiersz = 1;
+            while (wiersz <= Wiersze)
+            {
+                for (int s = Wiersze - wiersz; s > 0; s--)
+                    Console.Write(" ");
+                for (int g = 1; g <= wiersz; ++g)
+                    Console.Write("*");
+                Console.WriteLine();
+                wiersz++;
+
+            }
+
+
+        }
+
+        /*
+1|****     C) |
+2| ***        |
+3|  **        |
+4|   *        
+
+*/
+        public void GwiazdkiC()
+        {
+            int wiersz = 1;
+            while (wiersz <= Wiersze)
+            {
+                for (int s = 1; s < wiersz; s++)
+                    Console.Write(" ");
+                for (int g = Wiersze - wiersz + 1; g >= 1; --g)
+                    Console.Write("*");
+                Console.WriteLine();
+                wiersz++;
+
+            }
+
+        }
+
+        /*
+1|****     B) |
+2|***         |
+3|**          |
+4|*           
+*/
+        public void GwiazdkiB()
+        {
+            int wiersz = 1;
+            while (wiersz <= Wiersze)
+            {
+                for (int g = Wiersze - wiersz + 1; g >= 1; --g)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+                wiersz++;
+            }
+
+
+
+        }
+
+        /*
++------------+
+1  |*        A) |
+2  |**          |
+3  |***         |
+4  |****        |*/
+
+        public void GwiazdkiA()
+        {
+            int wiersz = 1;
+            while (wiersz <= Wiersze)
+            {
+                for (int g = 1; g <= wiersz; ++g)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+                wiersz = wiersz + 1;
+            }
+        }
+        public void Linia()
+        {
+            Console.WriteLine("===");
+
+        }
+    }
+}
--- naukacsharp/n05-gwiazdki/Program.cs
+++ naukacsharp/n05-gwiazdki/Program.cs
@@ -26,117 +26,21 @@ namespace n05_gwiazdki
     */
     class Program
     {
-        const int Wiersze = 3;
+        
         static void Main(string[] args)
         {
             Console.WriteLine("gwiazdki");
-            Linia();
-            GwiazdkiA();
-            Linia();
-            GwiazdkiB();
-            Linia();
-            GwiazdkiC();
-            Linia();
-            GwiazdkiD();
-            Linia();
+            Gwiazdki gw = new Gwiazdki();
+            gw.Linia();
+            gw.GwiazdkiA();
+            gw.Linia();
+            gw.GwiazdkiB();
+            gw.Linia();
+            gw.GwiazdkiC();
+            gw.Linia();
+            gw.GwiazdkiD();
+            gw.Linia();
            }
-        /*
-       ------------+
-     1|   *     D) |
-     2|  **        |
-     3| ***        |
-     4|****        |
-        */
 
-        private static void GwiazdkiD()
-        {
-            int wiersz = 1;
-            while (wiersz <= Wiersze)
-            {
-                for (int s = Wiersze - wiersz; s > 0; s--)
-                    Console.Write(" ");
-                for (int g = 1; g <= wiersz; ++g)
-                    Console.Write("*");
-                Console.WriteLine();
-                wiersz++;
-
-            }
-
-
-        }
-
-        /*
-1|****     C) |
-2| ***        |
-3|  **        |
-4|   *        
-
-*/
-        private static void GwiazdkiC()
-        {
-            int wiersz = 1;
-            while (wiersz <= Wiersze)
-            {
-                for (int s = 1; s < wiersz; s++)
-                    Console.Write(" ");
-                for (int g = Wiersze - wiersz + 1; g >= 1; --g)
-                    Console.Write("*");
-                Console.WriteLine();
-                wiersz++;
-
-            }
-
-        }
-
-        /*
-1|****     B) |
-2|***         |
-3|**          |
-4|*           
-*/
-        private static void GwiazdkiB()
-        {
-            int wiersz = 1;
-            while (wiersz <= Wiersze)
-            {
-                for (int g = Wiersze - wiersz + 1; g >= 1; --g)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-
-                wiersz++;
-            }
-
-
-
-        }
-
-        /*
-+------------+
-1  |*        A) |
-2  |**          |
-3  |***         |
-4  |****        |*/
-
-        private static void GwiazdkiA()
-        {
-            int wiersz = 1;
-            while (wiersz <= Wiersze)
-            {
-                for (int g = 1; g <= wiersz; ++g)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-
-                wiersz = wiersz + 1;
-            }
-        }
-        private static void Linia()
-        {
-            Console.WriteLine("===");
-
-        }
     }
 }
