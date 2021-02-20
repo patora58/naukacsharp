43c5dc190e20c78e87653b1cb5cb52a833228a7c
patora58 <patora58@gmail.com>
2021-02-20 21:40:32 +0100

    Magazyn-tablice

---
+++ naukacsharp/n06-mag-tablice/Dane.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace n06_mag_tablice
+{
+    class Dane
+    {
+        public int LicznikTowaru = 0;
+        public string[] NazwaTowaru = new string[20];
+        public int[] StanTowaru = new int[20];
+        public double[] CenaNetto = new double[20];
+        public double[] StawkaVat = new double[20];
+        public double[] CenaBrutto = new double[20];
+
+        public Dane()
+        {
+            NazwaTowaru[0] = "Szczotka";
+            StanTowaru[0] = 15;
+            CenaNetto[0] = 29.99;
+            StawkaVat[0] = 23.0;
+            LicznikTowaru++;
+
+            NazwaTowaru[1] = "Grzebień";
+            StanTowaru[1] = 10;
+            CenaNetto[1] = 15.00;
+            StawkaVat[1] = 7.5;
+            LicznikTowaru++;
+
+        }
+        public void DodajTowar(string nazwa, int stan, double netto, double vat)
+        {
+            NazwaTowaru[LicznikTowaru] = nazwa;
+            StanTowaru[LicznikTowaru] = stan;
+            CenaNetto[LicznikTowaru] = netto;
+            StawkaVat[LicznikTowaru] = vat;
+            LicznikTowaru++;
+        }
+    }
+}
---
+++ naukacsharp/n06-mag-tablice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace n06_mag_tablice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Magazyn - tablice");
+            Dane magazyn = new Dane();
+            magazyn.DodajTowar("Miska", 9, 10.00, 23.0);
+            magazyn.DodajTowar("Zabawka", 20, 5, 7.5);
+            PoliczBrutto(magazyn);
+            Wyswietl(magazyn);
+
+        }
+
+        private static void PoliczBrutto(Dane magazyn)
+        {
+            for (int i = 0; i < magazyn.LicznikTowaru; i++)
+            {
+                magazyn.CenaBrutto[i] = magazyn.CenaNetto[i]
+                * (1.0 + magazyn.StawkaVat[i] / 100.0); 
+            }
+        }
+
+        private static void Wyswietl(Dane magazyn)
+        {
+            for (int i = 0; i<magazyn.LicznikTowaru; i++)
+            {
+                Console.WriteLine($"{i}. {magazyn.NazwaTowaru[i]} "
+                    + $"\t Stan: {magazyn.StanTowaru[i]}"
+                    + $"\t netto: {magazyn.CenaNetto[i]}"
+                    +$"\t Vat: {magazyn.StawkaVat[i]}%"
+                    +$"\t Brutto: {magazyn.CenaBrutto[i]}"); 
+            }
+        }
+    }
+}
