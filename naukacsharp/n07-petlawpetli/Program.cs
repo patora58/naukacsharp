a51efcf92249b47734c9da240da6dfac4db6861a
patora58 <patora58@gmail.com>
2021-02-27 20:11:24 +0100

    Petla w petli

---
+++ naukacsharp/n07-petlawpetli/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+/* 1:1     1:2  1:3 
+ *2:1   2:2     2:3 
+*/
+namespace n07_petlawpetli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Czesc Petla");
+            for (int w = 1; w <= 9; w++) 
+            {
+                for (int k = 1; k <= 6; k++)
+                {
+                    Console.Write($"\t{w}:{k}");
+                }
+                Console.WriteLine();
+            }
+
+                
+
+
+        }
+    }
+}
